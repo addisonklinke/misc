88d1ebf7f4df43c80141469ffab8bfdd4509699f
Addison Klinke <agk38@case.edu>
2018-07-23 18:42:44 -0600

    update docstrings and comments, fix Python 2 to 3 errors

--- snow-forecasting/ReadSnowData.py
+++ snow-forecasting/ReadSnowData.py
@@ -1,29 +1,44 @@
+import re
+from urllib.request import urlopen
+import pandas as pd
+from MultipleReplace import MultipleReplace
+
 def ReadSnowData(state, siteID, eCodes):
 
-    # example:
-    # https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/start_of_period/1278:UT:SNTL%7Cid=%22%22%7Cname/POR_BEGIN,POR_END/TAVG::value,TMAX::value,TMIN::value,TOBS::value,PREC::value,SNWD::value,SNWDV::value,SNWDX::value,SNWDN::value,WTEQ::value,WTEQV::value,WTEQX::value,WTEQN::value,SMS:-2:value,SMS:-8:value,SMS:-20:value,STV::value,STX::value,STN::value,STO:-2:value,STO:-8:value,STO:-20:value
-    # Note: POR stands for 'period of record'
-    
-    import urllib, re
-    import pandas as pd
-    
-    def MultipleReplace(text, adict):
-        rx = re.compile('|'.join(map(re.escape, adict)))
-        def one_xlat(match):
-            return adict[match.group(0)]
-        return rx.sub(one_xlat, text)
-    
-    combined = siteID.split(':')[::-1]
-    combined = combined[0] + ':' + state + ':' + combined[1]
-    baseURL = 'https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/'
-    fullURL = baseURL + 'daily/start_of_period/' + combined + '%7Cid=%22%22%7Cname/POR_BEGIN,POR_END/' + eCodes
+    """ Download snow and weather data from Dept of Agriculture's API.
 
-    f = urllib.urlopen(fullURL)
-    raw = f.read()
+    Visit https://wcc.sc.egov.usda.gov/reportGenerator/ to see the form for generating URL requests. 
+    Note that the Department has made small changes to the URL format in the past, so the exact
+    formulation in this function may become deprecated in the future.
+
+    Parameters
+    ----------
+    state : str
+      The standard two letter abbreviation
+    siteID : str
+      ID code from snow-meta.csv file which for most cases will be of the form 'SNTL:xxxx'
+    eCodes : list of str
+      Capital letter variable abbreviations defined by the API 
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe containing all variables specified in `eCodes` for the given `state` and `siteID`
+    """
+    
+    # Generate request URL and read raw data
+    urlID = 'id=%22{}%22%20AND%20state=%22{}'.format(siteID.split(':')[-1], state)
+    baseURL = 'https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/start_of_period/'
+    fullURL = baseURL + urlID + '%22%20AND%20outServiceDate=%222100-01-01%22%7Cname/POR_BEGIN,POR_END/' + eCodes
+    f = urlopen(fullURL)
+    raw = f.read().decode('utf-8')
+    f.close()
+
+    # Split text into lines and remove all headers starting with '#'
     lines = raw.split('\n')
     data = [i for i in lines if not re.compile('^#').search(i)]
 
-    if data[1] != '':
+    if data[0] is not '':
         df = pd.DataFrame([s.split(',') for s in data[1:]])
         header = data[0].lower()
         subs = {'temperature':'temp', 'precipitation':'precip', 'equivalent':'equiv', 'moisture':'moist', 'accumulation':'accum',
@@ -31,7 +46,7 @@ def ReadSnowData(state, siteID, eCodes):
                 'start of day values':'', 'observed':'', 'percent':'', 'air ':'', 'depth':'', '-':'', '^[ \t\r\n]+|[ \t\r\n]+$':''}
         header = MultipleReplace(header, subs)
         df.columns = header.split(',')
-        df['id'] = pd.Series(siteID, index = df.index)
+        df['id'] = pd.Series(siteID, index=df.index)
         return df
     else:
         return None
