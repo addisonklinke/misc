081f947bcb1602c1223ccfa4e1b32588a4f326d1
addisonklinke <agk38@case.edu>
2018-07-26 13:53:04 -0600

    part way through EDA

--- kickstarter/kickstarter-eda.py
+++ kickstarter/kickstarter-eda.py
@@ -11,7 +11,10 @@ EDA and visualization for the Kickstarter projects dataset from Kaggle
 
 # Load required modules and data ----------------------------------------------
 import pandas as pd
+import numpy as np
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 os.chdir('/home/addisonklinke/Documents/git/portfolio/kickstarter') 
 df = pd.read_csv('ks-projects.csv', parse_dates=[5, 7])
@@ -63,11 +66,70 @@ df.to_csv('ks-projects-clean.csv', index=False)
 # Exploratory Analysis --------------------------------------------------------
 df = pd.read_csv('ks-projects-clean.csv', parse_dates=[4, 11, 12])
 
-# How are projects distributed by category and sub-category?
+# How are projects distributed by category? 
+main_categories = df.groupby('main_category').size().sort_values(ascending=False)
+n_main = [i / 1000 for i in list(main_categories)]
+labels_main = list(main_categories.index)
 
-# Variable correlations
+ind = np.arange(len(main_categories))
+plt.bar(ind, n_main)
+plt.ylabel('Count (Thousands)')
+plt.xlabel('Main Category')
+plt.title('Kickstarter Project Categories')
+plt.xticks(ind, labels_main, rotation=90)
+plt.show()
+
+# Correlations for numeric variables
+smaller = df[['goal_usd', 'pledged_usd', 'backers', 'duration', 'state']]
+smaller.corr().unstack().sort_values().drop_duplicates()
+# Strong correlation between pledged_usd and backers, which is to be expected
+# since the more people backing the project, the more money it is likely to have
+# raised. Other than this, correlations are fairly small
 
 # How does sucess vary with factors like category, goal, country, duration,
-# and time of day the project was launched?
+# and time of day the project was launched? For duration, there are a few 
+# outliers with over 14,000 days resulting from incorrect parsing of the launch
+# date, so we will remove those first.
+df.query('duration < 1000', inplace=True)
+sns.distplot(df.query('state == 1').duration, hist=False, kde=True,
+             kde_kws={'shade': True, 'linewidth': 3}, 
+             label='Success', color='green')
+sns.distplot(df.query('state == 0').duration, hist=False, kde=True,
+             kde_kws={'shade': True, 'linewidth': 3}, 
+             label='Failure', color='red') 
+plt.legend(title='Project Status')
+plt.xlabel('Project Duration (Days)')
+plt.ylabel('Density')
+plt.show()
+# The vast majority of projects are 30 days long regardless of their status.
+# The next largest peaks are at 60 and 45 days, indicating that projects tend 
+# to be scheduled in terms of months (1, 1.5, 2, etc). There is a slight trend
+# towards shorter durations leading to an increased chance of success - more 60
+# day projects failed and more 15-25 day project succeeded.
 
-# Insights on project name choices?
+# Now we can look at time of day the project funding goal (converted to USD for
+# a fair comparison across all the currencies). Plotting on a log10 scale helps 
+# compress the data into a viewable range since goals range from $0.01 up to 
+# over $166,000,000
+sns.distplot(np.log10(df.query('state == 1').goal_usd.values), hist=False, 
+             kde=True, kde_kws={'shade': True, 'linewidth': 3}, 
+             label='Success', color='green')
+sns.distplot(np.log10(df.query('state == 0').goal_usd.values), hist=False, 
+             kde=True, kde_kws={'shade': True, 'linewidth': 3}, 
+             label='Failure', color='red') 
+plt.legend(title='Project Status')
+plt.xlabel('Project Funding Goal (log10 USD)')
+plt.ylabel('Density')
+plt.show()
+
+# The distinction between failure and sucess is a more pronounced here than for
+# duration - project with a larger funding goal failed more often. The most 
+# common goals were around $10k USD
+
+# Insights on project name choices from an NLP perspective?
+
+# Initial Modeling ------------------------------------------------------------
+
+# Ensemble Modeling -----------------------------------------------------------
+
+# Export Final Model ----------------------------------------------------------
