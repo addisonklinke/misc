35b70cf1648c5294ce2b644189aaa28fd46a1e39
addisonklinke <agk38@case.edu>
2018-08-13 22:34:17 -0600

    transfering kickstarter to Jupyter

--- kickstarter/kickstarter-eda.py
+++ kickstarter/kickstarter-eda.py
@@ -13,12 +13,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 os.chdir('/home/addisonklinke/Documents/git/portfolio/kickstarter') 
-df = pd.read_csv('ks-projects.csv', parse_dates=[5, 7])
+raw = pd.read_csv('ks-projects.csv', parse_dates=[5, 7])
 
 # Data cleaning ---------------------------------------------------------------
-df.shape
-df.describe()
-df.dtypes
+raw.shape
+raw.describe()
+raw.dtypes
 # First, we notice that there are two columns for the amount of money pledged:
 # 'usd pledged' and 'usd_pledged_real'. According to documentation for the 
 # dataset, the former was converted by Kickstarter and the later by the 
@@ -29,34 +29,32 @@ df.dtypes
 
 # We will also drop the project ID since that is a random number which should
 # have no correlation to the project's success.
-df.drop(columns=['usd pledged', 'ID'], inplace=True)
-df.rename(columns={'usd_pledged_real':'pledged_usd', 
+clean = raw.drop(columns=['usd pledged', 'ID'])
+clean.rename(columns={'usd_pledged_real':'pledged_usd', 
                    'usd_goal_real':'goal_usd'}, inplace=True)
 
-df.groupby('state').size()
+clean.groupby('state').size()
 # The majority of projects are either failed of successful. Canceled is the 
 # next biggest category. We can simplify this to a binary classification
 # problem with failed or canceled as 0 and successful as 1.
 
 # Remove the unneeded rows and enocde classes
-df = df[df.state.isin(['successful', 'failed', 'canceled'])]
-df.state = df.state.replace({'successful':1, 'failed':0, 'canceled':0})
+clean = clean[clean.state.isin(['successful', 'failed', 'canceled'])]
+clean.state = clean.state.replace({'successful':1, 'failed':0, 'canceled':0})
 
 # Add a 'duration' variable which measures the length of the project. We will
 # also split 'launched' into the day and time
-df['launch_day'] = [d.date() for d in df.launched]
-df['launch_time'] = [d.time() for d in df.launched]
-df['duration'] = [(d - l).days for d, l in zip(df.deadline.dt.date, df.launch_day)]
-df.drop(columns='launched', inplace=True)
-np.unique(df.launch_day.dt.year)
-
-# TODO: drop the 1970's rows
+clean['launch_day'] = [d.date() for d in clean.launched]
+clean['launch_time'] = [d.time() for d in clean.launched]
+clean['duration'] = [(d - l).days for d, l in zip(clean.deadline.dt.date, clean.launch_day)]
+clean.drop(columns='launched', inplace=True)
+np.unique(clean.launch_day.dt.year)
 
 # Split launch_time into hours, minutes, and seconds and convert to a single
 # decimal respresentation. 
-df[['h', 'm', 's']] = df.launch_time.astype(str).str.split(':', expand=True).astype(float)
-df['launch_time'] = df.h.values + df.m.values / 60 + df.s.values / 3600
-df.drop(columns=['h', 'm', 's'], inplace=True)
+clean[['h', 'm', 's']] = clean.launch_time.astype(str).str.split(':', expand=True).astype(float)
+clean['launch_time'] = clean.h.values + clean.m.values / 60 + clean.s.values / 3600
+clean.drop(columns=['h', 'm', 's'], inplace=True)
 
 # We can also encode launch_day and deadline as integers representing day of 
 # the year. Both of these transformations will make the data more suitable for 
@@ -66,19 +64,19 @@ df.drop(columns=['h', 'm', 's'], inplace=True)
 
 # We will also move the 'state' column last so that splitting features and 
 # target will be easy in sklearn
-cols = list(df.columns)
+cols = list(clean.columns)
 cols.append(cols.pop(cols.index('state')))
-df = df.reindex(columns=cols)
+clean = clean.reindex(columns=cols)
 
 # Finally, write the modified dataframe to CSV so we don't have to repeat these
 # steps in the future
-df.to_csv('ks-projects-clean.csv', index=False)
+clean.to_csv('ks-projects-clean.csv', index=False)
 
 # Exploratory Analysis --------------------------------------------------------
-df = pd.read_csv('ks-projects-clean.csv', parse_dates=[4, 11, 12])
+clean = pd.read_csv('ks-projects-clean.csv', parse_dates=[4, 11, 12])
 
 # How are projects distributed by category? 
-main_categories = df.groupby('main_category').size().sort_values(ascending=False)
+main_categories = clean.groupby('main_category').size().sort_values(ascending=False)
 n_main = [i / 1000 for i in list(main_categories)]
 labels_main = list(main_categories.index)
 
@@ -91,7 +89,7 @@ plt.xticks(ind, labels_main, rotation=90)
 plt.show()
 
 # Correlations for numeric variables
-smaller = df[['goal_usd', 'pledged_usd', 'backers', 'duration', 'state']]
+smaller = clean[['goal_usd', 'pledged_usd', 'backers', 'duration', 'state']]
 smaller.corr().unstack().sort_values().drop_duplicates()
 # Strong correlation between pledged_usd and backers, which is to be expected
 # since the more people backing the project, the more money it is likely to have
@@ -101,11 +99,11 @@ smaller.corr().unstack().sort_values().drop_duplicates()
 # and time of day the project was launched? For duration, there are a few 
 # outliers with over 14,000 days resulting from incorrect parsing of the launch
 # date, so we will remove those first.
-df.query('duration < 1000', inplace=True)
-sns.distplot(df.query('state == 1').duration, hist=False, kde=True,
+clean.query('duration < 1000', inplace=True)
+sns.distplot(clean.query('state == 1').duration, hist=False, kde=True,
              kde_kws={'shade': True, 'linewidth': 3}, 
              label='Success', color='green')
-sns.distplot(df.query('state == 0').duration, hist=False, kde=True,
+sns.distplot(clean.query('state == 0').duration, hist=False, kde=True,
              kde_kws={'shade': True, 'linewidth': 3}, 
              label='Failure', color='red') 
 plt.legend(title='Project Status')
@@ -122,10 +120,10 @@ plt.show()
 # a fair comparison across all the currencies). Plotting on a log10 scale helps 
 # compress the data into a viewable range since goals range from $0.01 up to 
 # over $166,000,000
-sns.distplot(np.log10(df.query('state == 1').goal_usd.values), hist=False, 
+sns.distplot(np.log10(clean.query('state == 1').goal_usd.values), hist=False, 
              kde=True, kde_kws={'shade': True, 'linewidth': 3}, 
              label='Success', color='green')
-sns.distplot(np.log10(df.query('state == 0').goal_usd.values), hist=False, 
+sns.distplot(np.log10(clean.query('state == 0').goal_usd.values), hist=False, 
              kde=True, kde_kws={'shade': True, 'linewidth': 3}, 
              label='Failure', color='red') 
 plt.legend(title='Project Status')
@@ -140,7 +138,7 @@ plt.show()
 # What about time of day the project was launched? If people are most likely
 # to hear about a project at its inception, it would be critical to launch at
 # a time when people are available (for instance, weekday evenings).
-smaller = df[['launch_time', 'state']]
+smaller = clean[['launch_time', 'state']]
 smaller['h']
 sns.tsplot()
 
